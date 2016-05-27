b0cbe8619c2f3f4d926639d72b8beaf0d292c3ca
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-05-27 14:54:08 +0700

    no message

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -1,62 +1,196 @@
 #include <ESPert.h>
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+
+extern "C" {
+#include "ets_sys.h"
+#include "os_type.h"
+#include "osapi.h"
+#include "mem.h"
+#include "user_interface.h"
+#include "cont.h"
+}
 
 ESPert espert;
-const char *host = "api.thingspeak.com"; // ส่งค่าไปยังเว็บ thingspeak
-const int httpPort = 80;
+
+#define DHTPIN 12     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+#define DEBUG
+
+#define DEBUG_PRINTER Serial
+
+#ifdef DEBUG
+#define DEBUG_PRINT(...) { DEBUG_PRINTER.print(__VA_ARGS__); }
+#define DEBUG_PRINTLN(...) { DEBUG_PRINTER.println(__VA_ARGS__); }
+#else
+#define DEBUG_PRINT(...) {}
+#define DEBUG_PRINTLN(...) {}
+#endif
+
+const char* ssid     = "@ESPertAP_001";
+const char* password = "espertap";
+
+DHT *dht;
+
+const int sleepTimeS = 30;
+
+void connectWifi();
+void reconnectWifiIfLinkDown();
+void initDht(DHT **dht, uint8_t pin, uint8_t dht_type);
+void readDht(DHT *dht, float *temp, float *humid);
+void uploadThingsSpeak(float t, float h);
 
 void setup() {
-  // ประกาศใช้งานทั่วไป
-  espert.init();
-  espert.oled.init();
-  espert.dht.init();
-  delay(2000);
+  unsigned long rtc_time = system_get_rtc_time();
+  unsigned long clock_time = system_get_time();
+  rst_info* info = system_get_rst_info();
+  //rst_info* info = ESP.getResetInfoPtr();
+  int reason = info->reason;
+  int cause = info->exccause;
+  
+  Serial.begin(115200);
+  delay(10);
+  
+  connectWifi();
 
-  espert.oled.clear();
-  espert.oled.println(espert.info.getId());
-  espert.oled.println();
-
-  int mode = espert.wifi.init();
-
-  if (mode == ESPERT_WIFI_MODE_CONNECT) {
-    espert.println(">>> WiFi mode: connected.");
-    espert.oled.println("WiFi: connected.");
-    espert.oled.print("IP..: ");
-    espert.oled.println(espert.wifi.getLocalIP());
-  } else if (mode == ESPERT_WIFI_MODE_DISCONNECT) {
-    espert.println(">>> WiFi mode: disconnected.");
-    espert.oled.println("WiFi: not connected.");
-  } else if (mode == ESPERT_WIFI_MODE_SMARTCONFIG) {
-    espert.println(">>> WiFi mode: smart config.");
-  } else if (mode == ESPERT_WIFI_MODE_SETTINGAP) {
-    espert.println(">>> WiFi mode: access point.");
-  }
+  initDht(&dht, DHTPIN, DHTTYPE);
 }
 
 void loop() {
-  espert.loop();
-  bool isFarenheit = false;
-  float t = espert.dht.getTemperature(isFarenheit); // อ่านค่าอุณหภูมิ
-  float h = espert.dht.getHumidity();               // อ่านค่าความชื้น
+  static float t_dht;
+  static float h_dht;
 
-  // ฟังก์ชั่นตรวจสอบเซ็นเซอร์ว่ายังสามารถอ่านค่าได้รึป่าว แล้วแสดงผลออกจอ OLED 
-  if (!isnan(t) && !isnan(h)) {
-    String dht = "Temperature: " + String(t) + (isFarenheit ?  " F" : " C") + "\n";
-    dht += "Humidity...: " + String(h) + " %\n";
-    espert.oled.clear();
-    espert.oled.setTextSize(2);
-    espert.oled.setTextColor(ESPERT_WHITE);
-    espert.oled.println(dht);
-    espert.println(dht);
-  }
+  readDht(dht, &t_dht, &h_dht);
+  uploadThingsSpeak(t_dht, h_dht);
   
-  // ทำการอัพค่าขึ้น thingspeak
-  //                                    ส่งค่าไปยัง API Key 1DYAF3DO3X8GR6JS
-  //const String path = String("/update?api_key=1DYAF3DO3X8GR6JS&field1=") += String(t)  // field1 ให้ส่งค่าจากตัวแปร t
-  //                    += String("&field2=") += String(h);                              // field2 ให้ส่งค่าจากตัวแปร h
-  const String path = String("/update?api_key=1DYAF3DO3X8GR6JS&field1=") += String(t) += String("&field2=") += String(h);
-  
-  espert.println( "Connecting..." );
-  espert.println( ">>" + espert.wifi.getHTTP( host, path.c_str() ) + "<<" ); // ส่งค่าไปโล้ดดดด
-  delay( 1000 );
+  ESP.deepSleep(sleepTimeS * 6000000);
+
+  reconnectWifiIfLinkDown();
 }
 
+void reconnectWifiIfLinkDown() {
+  if (WiFi.status() != WL_CONNECTED) {
+    DEBUG_PRINTLN("WIFI DISCONNECTED");
+    connectWifi();
+  }
+}
+
+void connectWifi() {
+  DEBUG_PRINTLN();
+  DEBUG_PRINTLN();
+  DEBUG_PRINT("Connecting to ");
+  DEBUG_PRINTLN(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DEBUG_PRINT(".");
+  }
+
+  DEBUG_PRINTLN("");
+  DEBUG_PRINTLN("WiFi connected");
+  DEBUG_PRINTLN("IP address: ");
+  DEBUG_PRINTLN(WiFi.localIP());
+}
+
+void initDht(DHT **dht, uint8_t pin, uint8_t dht_type) {
+  // Connect pin 1 (on the left) of the sensor to +5V
+  // NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+  // to 3.3V instead of 5V!
+  // Connect pin 2 of the sensor to whatever your DHTPIN is
+  // Connect pin 4 (on the right) of the sensor to GROUND
+  // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+  // Initialize DHT sensor for normal 16mhz Arduino
+  // NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
+  // might need to increase the threshold for cycle counts considered a 1 or 0.
+  // You can do this by passing a 3rd parameter for this threshold.  It's a bit
+  // of fiddling to find the right value, but in general the faster the CPU the
+  // higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+  // Arduino Due that runs at 84mhz a value of 30 works.
+  // Example to initialize DHT sensor for Arduino Due:
+  //DHT dht(DHTPIN, DHTTYPE, 30);
+
+  *dht = new DHT(pin, dht_type, 30);
+  (*dht)->begin();
+  DEBUG_PRINTLN(F("DHTxx test!"))  ;
+}
+
+void uploadThingsSpeak(float t, float h) {
+  espert.led.on();
+  static const char* host = "api.thingspeak.com";
+  static const char* apiKey = "QVLUV8I946J3TG6Z";
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    DEBUG_PRINTLN("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/update/";
+  //  url += streamId;
+  url += "?key=";
+  url += apiKey;
+  url += "&field1=";
+  url += t;
+  url += "&field2=";
+  url += h;
+
+  DEBUG_PRINT("Requesting URL: ");
+  DEBUG_PRINTLN(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+  delay(500);               
+  espert.led.off();
+}
+
+void readDht(DHT *dht, float *temp, float *humid) {
+
+  if (dht == NULL) {
+    DEBUG_PRINTLN(F("[dht22] is not initialised. please call initDht() first."));
+    return;
+  }
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht->readHumidity();
+
+  // Read temperature as Celsius
+  float t = dht->readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht->readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    DEBUG_PRINTLN("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht->computeHeatIndex(f, h);
+
+  DEBUG_PRINT("Humidity: ");
+  DEBUG_PRINT(h);
+  DEBUG_PRINT(" %\t");
+  DEBUG_PRINT("Temperature: ");
+  DEBUG_PRINT(t);
+  DEBUG_PRINT(" *C ");
+  DEBUG_PRINT(f);
+  DEBUG_PRINT(" *F\t");
+  DEBUG_PRINT("Heat index: ");
+  DEBUG_PRINT(hi);
+  DEBUG_PRINTLN(" *F");
+
+  *temp = t;
+  *humid = h;
+
+}
